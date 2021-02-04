6684f1b780613026c46d4a832033f432b24b68a3
stivipp <stivippx@gmail.com>
2021-02-04 13:44:51 +0100

    Using data.js as DB/Back-End, basic showing data using ARRAY of OBJECTS in v-for cycle.

---
+++ src/data.js
@@ -0,0 +1,63 @@
+/*
+** For this purpose we are playing like this is our DB, our BACK-END. :)
+** Array of OBJECTS.
+*/
+
+export const data = [
+    {
+        id: 1,
+        title: 'FREMENI',
+        cards: [
+            {
+                id: 1,
+                text: 'Pustny Ludia'
+            },
+            {
+                id: 2,
+                text: 'Je ich viac ako 1000000'
+            },
+            {
+                id: 3,
+                text: 'Korenie Tajomstvo - Melanch'
+            }
+        ]
+      },
+
+      {
+        id: 2,
+        title: 'ATREIDI',
+        cards: [
+            {
+                id: 1,
+                text: 'Nice cool ludia :D'
+            },
+            {
+                id: 2,
+                text: 'Cestny'
+            },
+            {
+                id: 3,
+                text: 'Maju Muad Diba a Leta II a su velmi silny :D.'
+            }
+        ]
+      },
+
+      {
+        id: 3,
+        title: 'HARKONENOVCI',
+        cards: [
+            {
+                id: 1,
+                text: 'Bohaty velmi'
+            },
+            {
+                id: 2,
+                text: 'Velmi zly ludia... '
+            },
+            {
+                id: 3,
+                text: 'Su drzy a nakoniec ich vsetkych zabiju.'
+            }
+        ]
+      }
+]
