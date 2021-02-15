da6c876e12e572da38281f53856620504590b60f
stivipp <stivippx@gmail.com>
2021-02-15 12:14:22 +0100

    Using first Reactivity, v-for, @click.

---
+++ src/components/ListCreateForm.vue
@@ -0,0 +1,18 @@
+<template>
+    <form class="flex-shrink-0 sm:w-80 sm:mx-4 sm:mb-0 sm:pr-8 text-gray-800 " action="">
+        <input class="w-full bg-white bg-opacity-40 focus:bg-opacity-100 font-bold placeholder-white focus:placeholder-gray-600 p-2.5 rounded-lg outline-none focus:shadow-xl" type="text" placeholder="Add another list" control-id="ControlID-3">
+    </form>
+
+</template>
+
+
+
+<script>
+export default {
+    setup () {
+        
+
+        return {}
+    }
+}
+</script>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,7 +2,13 @@
   <main class="flex h-screen bg-purple-500 lg:bg-gradient-to-r from-red-500 to-pink-400">
     <div class="sm:flex items-start w-screen px-4 py-10 overflow-x-auto">
 
-      <List v-for="list in lists" :title="list.title" :cards="list.cards" :key="list.id" />
+      <List v-for="list in lists"
+       :title="list.title"
+       :cards="list.cards" 
+       :key="list.id"
+      />
+
+      <ListCreateForm />
 
     </div>
   </main>
@@ -13,11 +19,13 @@ import { ref } from 'vue'
 import { data } from '@/data.js'
 
 import List from '@/components/List.vue'
+import ListCreateForm from '@/components/ListCreateForm.vue'
 
 
 export default {
   components: {
-    List
+    List,
+    ListCreateForm
   },
 
   setup() {
